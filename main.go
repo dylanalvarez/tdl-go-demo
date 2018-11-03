e06e3aa3a5b6fbdcd186927b1679f04d083b1709
Dylan Alvarez <helllo5d123@gmail.com>
2018-11-02 21:36:12 -0300

    ejemplo parte 1

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
-import (
-    "example/routes"
-    "github.com/gin-gonic/gin"
-)
+import "fmt"
+
+func calculateFibonacci(n int) {
+	var x int
+	y := 1
+	for i := 0; i < n; i++ {
+		x, y = y, x+y
+		fmt.Println(x)
+	}
+}
+
+func calculateFactorials(n int) {
+	accumulator := 1
+	fmt.Println(accumulator)
+	for i := 1; i < n; i++ {
+		accumulator *= i
+		fmt.Println(accumulator)
+	}
+}
 
 func main() {
-    engine := gin.Default()
-    routes.Setup(engine)
-    engine.Run()
+	calculateFactorials(10)
+	calculateFibonacci(10)
 }
